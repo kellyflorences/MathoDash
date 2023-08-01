f059ea98f09b9d931d39ed8016d7c50196e5e577
kellyflorences <77277696+kellyflorences@users.noreply.github.com>
2023-08-01 15:14:54 +0700

    Initial Commit

---
+++ MathoDash/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MathoDash
+//
+//  Created by Kelly Florences Tanjaya on 01/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MathoDash/MathoDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathoDashApp.swift
+//  MathoDash
+//
+//  Created by Kelly Florences Tanjaya on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MathoDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
